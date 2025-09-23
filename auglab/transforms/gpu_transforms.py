b1e84a81167832121b4d63fe1e8ae266223dd476
Nathan Molinier <nathan.molinier@gmail.com>
2025-09-23 10:33:33 -0400

    apply affine transform to mask

--- auglab/transforms/gpu_transforms.py
+++ auglab/transforms/gpu_transforms.py
@@ -1,14 +1,16 @@
 import os, json
 
-import kornia as K
 from kornia.augmentation import RandomAffine3D
 from kornia.augmentation import AugmentationSequential
 from kornia.constants import Resample
+from kornia.core import Tensor
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import torchvision.transforms._functional_tensor as F_t
 from torch.distributions import Bernoulli
+
+from typing import Any, Dict, Optional
 
 class DataAugmentationGPU(AugmentationSequential):
     """
@@ -70,6 +72,31 @@ class DataAugmentationGPU(AugmentationSequential):
             keepdim=False
         ))
         return transforms
+
+class RandomAffine3DCustom(RandomAffine3D):
+    def apply_non_transform_mask(
+        self, input: Tensor, params: Dict[str, Tensor], flags: Dict[str, Any], transform: Optional[Tensor] = None
+    ) -> Tensor:
+        """Process masks corresponding to the inputs that are no transformation applied."""
+        return input
+
+    def apply_transform_mask(
+        self, input: Tensor, params: Dict[str, Tensor], flags: Dict[str, Any], transform: Optional[Tensor] = None
+    ) -> Tensor:
+        """Process masks corresponding to the inputs that are transformed.
+
+        Note:
+            Convert "resample" arguments to "nearest" by default.
+
+        """
+        resample_method: Optional[Resample]
+        if "resample" in flags:
+            resample_method = flags["resample"]
+            flags["resample"] = Resample.get("nearest")
+        output = self.apply_transform(input, params, flags, transform)
+        if resample_method is not None:
+            flags["resample"] = resample_method
+        return output
 
 class RandomTransformGPU(nn.Module):
     def __init__(self, transform: nn.Module, apply_probability: float = 1):
