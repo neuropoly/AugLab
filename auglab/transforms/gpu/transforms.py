fbf9cda46cefb100268da0c0689b1c6b373e7e9a
Nathan Molinier <nathan.molinier@gmail.com>
2025-10-28 11:56:50 -0400

    add code to test transforms

--- auglab/transforms/gpu/transforms.py
+++ auglab/transforms/gpu/transforms.py
@@ -85,4 +85,67 @@ class AugTransformsGPU(AugmentationSequentialCustom):
         ))
         return transforms
 
+if __name__ == "__main__":
+    # Example usage
+    import importlib
+    import auglab.configs as configs
+    from auglab.utils.image import Image
 
+    configs_path = importlib.resources.files(configs)
+    json_path = configs_path / "transform_params_gpu.json"
+    augmentor = AugTransformsGPU(json_path)
+
+    # Load image and mask tensors
+    import torch
+    img_path = '/home/GRAMES.POLYMTL.CA/p118739/data_nvme_p118739/data/datasets/data-multi-subject/sub-amu02/anat/sub-amu02_T1w.nii.gz'
+    img = Image(img_path).change_orientation('RSP')
+    img_tensor = torch.from_numpy(img.data.copy()).unsqueeze(0).to(torch.float32)
+
+    seg_path = '/home/GRAMES.POLYMTL.CA/p118739/data_nvme_p118739/data/datasets/data-multi-subject/derivatives/labels/sub-amu02/anat/sub-amu02_T1w_label-spine_dseg.nii.gz'
+    seg = Image(seg_path).change_orientation('RSP')
+    seg_tensor_all = torch.from_numpy(seg.data.copy()).unsqueeze(0)
+
+    # Add segmentation values to different channels
+    seg_tensor = torch.zeros((5, *seg_tensor_all.shape[1:]))
+    for i, value in enumerate([12, 13, 14, 15, 16]):
+        seg_tensor[i] = (seg_tensor_all == value)
+
+    # Format tensors to match expected input shape (B, C, D, H, W)
+    img_tensor = torch.cat([img_tensor, seg_tensor_all.bool().int()], dim=0).unsqueeze(0)  # Add batch dimension and seconda channel
+    seg_tensor = seg_tensor.unsqueeze(0)  # Add batch dimension
+
+    # Move to GPU
+    img_tensor = img_tensor.cuda()
+    seg_tensor = seg_tensor.cuda()
+    augmentor = augmentor.cuda()
+
+    # Apply augmentations
+    augmented_img, augmented_seg = augmentor(img_tensor.clone(), seg_tensor.clone())
+
+    if augmented_img.shape != img_tensor.shape:
+        raise ValueError("Augmented image shape does not match input shape.")
+    if augmented_seg.shape != seg_tensor.shape:
+        raise ValueError("Augmented segmentation shape does not match input shape.")
+    
+    import cv2
+    import numpy as np
+    # Convert tensors to numpy arrays
+    img_tensor_np = img_tensor.cpu().detach().numpy()
+    seg_tensor_np = seg_tensor.cpu().detach().numpy()
+    augmented_img_np = augmented_img.cpu().detach().numpy()
+    augmented_seg_np = augmented_seg.cpu().detach().numpy()
+
+    # Concatenate segmentation channels for visualization
+    seg_tensor_np = np.sum(seg_tensor_np, axis=1)
+    augmented_seg_np = np.sum(augmented_seg_np, axis=1)
+
+    # Save the augmented images
+    middle_slice = img_tensor_np.shape[2] // 2
+    os.makedirs('img', exist_ok=True)
+    cv2.imwrite('img/augmented_img.png', augmented_img_np[0, 0, middle_slice])
+    cv2.imwrite('img/not_augmented_channel.png', augmented_img_np[0, 1, middle_slice]*255)
+    cv2.imwrite('img/img.png', img_tensor_np[0, 0, middle_slice])
+    cv2.imwrite('img/augmented_seg.png', augmented_seg_np[0, middle_slice]*255)
+    cv2.imwrite('img/seg.png', seg_tensor_np[0, middle_slice]*255)
+
+    print(augmentor)
