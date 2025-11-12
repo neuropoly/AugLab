eef505e2906302328cb7075396557776882e4797
Nathan Molinier <nathan.molinier@gmail.com>
2025-11-11 22:02:25 -0500

    increase back number of epochs

--- auglab/trainers/nnUNetTrainerDAExt.py
+++ auglab/trainers/nnUNetTrainerDAExt.py
@@ -233,7 +233,6 @@ class nnUNetTrainerDAExtGPU(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict,
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, device)
-        self.num_epochs = 100
 
         # Load transform parameters from json file
         configs_path = importlib.resources.files(configs)
