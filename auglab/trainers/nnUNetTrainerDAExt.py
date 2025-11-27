607f9b6ecb23db6afa10cd6ff588fa7be476eb72
Nathan Molinier <nathan.molinier@gmail.com>
2025-11-27 11:48:30 -0500

    add redistribute seg transform

--- auglab/trainers/nnUNetTrainerDAExt.py
+++ auglab/trainers/nnUNetTrainerDAExt.py
@@ -27,11 +27,13 @@ import os
 import torch
 import importlib
 from torch import autocast
+import json
 
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.utilities.helpers import dummy_context
 
 from auglab.transforms.cpu.transforms import AugTransforms
+from auglab.transforms.cpu.fromSeg import RedistributeTransform
 import auglab.configs as configs
 from auglab.transforms.gpu.transforms import AugTransformsGPU
 from auglab.trainers.utils import DownsampleSegForDSTransformCustom
@@ -187,6 +189,21 @@ class nnUNetTrainerDAExtGPU(nnUNetTrainer):
             retain_stats: bool = False
     ) -> BasicTransform:
         transforms = []
+
+        # Load transform parameters for CPU transforms
+        configs_path = importlib.resources.files(configs)
+        json_path = os.environ.get("AUGLAB_PARAMS_GPU_JSON", str(configs_path / "transform_params_gpu.json"))
+        with open(json_path, 'r') as f:
+            transform_params = json.load(f)
+
+        # Add redistribute segmentation transform
+        redisSeg_params = transform_params.get('RedistributeSegTransform')
+        if redisSeg_params is not None:
+            transforms.append(RandomTransform(
+                RedistributeTransform(
+                    retain_stats=redisSeg_params.get('retain_stats', False),
+                ), apply_probability=redisSeg_params.get('probability', 0.5),
+            ))
 
         ### Keep some nnunet transforms
         if do_dummy_2d_data_aug:
